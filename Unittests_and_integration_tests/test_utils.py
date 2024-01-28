df9a31709fc9ed5c634a3d6d658bdae504fdfd28
evanrich2404 <evanrich2404@gmail.com>
2024-01-28 13:36:57 -0600

    task 1, testing for errors and if it catches said errors

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -18,8 +18,12 @@ memoize
 
 class TestAccessNestedMap(unittest.TestCase):
     """
-    test_access_nested_map method checks to see if it returns
-    what it is supposed to
+    Test cases for access_nested_map function:
+    "test_access_nested_map" method checks to see if it returns
+    what it is supposed to,
+    "test_access_nested_map_exception" method is checking for
+    errors or more specifically if it checks for errors properly,
+    ""
     """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -29,3 +33,12 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected)
+
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b")
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, missing_key):
+        with self.assertRaises(KeyError) as conman:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(conman.exception), repr(missing_key))
