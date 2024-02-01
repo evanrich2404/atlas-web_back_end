72e7c3dbdc129e57279c7e960bda1a2302728c13
evanrich2404 <evanrich2404@gmail.com>
2024-01-31 19:54:33 -0600

    task 4

---
+++ i18n/4-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+placeholder
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    """Config class for Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def home():
+    """template"""
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """babel location selection"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
