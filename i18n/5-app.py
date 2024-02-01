de026c1b5826ead46cc82c71949c86d0b53d5b87
evanrich2404 <evanrich2404@gmail.com>
2024-01-31 20:39:04 -0600

    nothing workin

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python3
-"""
-placeholder
-"""
+""" placeholder """
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
+app = Flask(__name__)
 
 
 class Config(object):
@@ -20,15 +13,9 @@ class Config(object):
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app = Flask(__name__)
 app.config.from_object(Config)
+
 babel = Babel(app)
-
-
-@app.route('/')
-def home():
-    """template"""
-    return render_template('5-index.html')
 
 
 @babel.localeselector
@@ -38,6 +25,14 @@ def get_locale():
     if locale and locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
 def get_user():
@@ -55,6 +50,12 @@ def before_request():
     """before request"""
     user = get_user
     g.user = user
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def home():
+    """template"""
+    return render_template('5-index.html')
 
 
 if __name__ == "__main__":
