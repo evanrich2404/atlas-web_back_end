a230c92e76df3851ba88d7512966347428a7c903
evanrich2404 <evanrich2404@gmail.com>
2024-01-31 22:46:08 -0600

    probably not gonna fix much since no difference in html

--- i18n/6-app.py
+++ i18n/6-app.py
@@ -60,7 +60,7 @@ def before_request():
 @app.route('/', methods=['GET'], strict_slashes=False)
 def home():
     """template"""
-    return render_template('5-index.html')
+    return render_template('6-index.html')
 
 
 if __name__ == "__main__":
