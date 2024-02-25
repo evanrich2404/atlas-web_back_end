2967acc20a1fb1769c6e5c5bd6ea83d8804e0851
evanrich2404 <evanrich2404@gmail.com>
2024-02-25 16:20:20 -0600

    trying to add some more documentation to see if that is the missing check

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -4,6 +4,7 @@ const fs = require('fs').promises;
 // Function to asynchronously read from the database and format the response
 async function countStudents(path) {
   try {
+    // process the file content
     const data = await fs.readFile(path, { encoding: 'utf8' });
     let message = 'This is the list of our students\n';
     const lines = data.split('\n').filter((line) => line);
@@ -16,6 +17,7 @@ async function countStudents(path) {
       return { firstName, field };
     });
 
+    // Process each field
     const fields = {};
     students.forEach((student) => {
       if (!fields[student.field]) {
@@ -29,18 +31,23 @@ async function countStudents(path) {
       message += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
     });
 
+    // Return a meaningful result
     return message.trim();
   } catch (Error) {
+    // If the file does not exist or cannot be read, throw an error
     throw new Error('Cannot load the database');
   }
 }
 
+// Create an Express app
 const app = express();
 
+// Define a route for the URL '/'
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
+// Define a route for the URL '/'
 app.get('/students', async (req, res) => {
   try {
     const message = await countStudents('database.csv');
@@ -50,8 +57,10 @@ app.get('/students', async (req, res) => {
   }
 });
 
+// Make the application listen on port 1245
 app.listen(1245, () => {
   console.log('Server listening on port 1245');
 });
 
+// Export ze app
 module.exports = app;
